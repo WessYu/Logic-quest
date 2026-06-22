c911e48cebb37655533aaf37815b10f954bef9ad
Wesley <wess.c@proton.me>
2026-06-22 08:06:04 -0300

    Render GitHub presence section

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import GitHubPresence from "./GitHubPresence";
 import "./styles.css";
 import "./stepAlignment.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <GitHubPresence />
   </React.StrictMode>,
 );
 
