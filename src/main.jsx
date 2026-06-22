229d348e5f9705e1f47f58a47da781349466cdc4
Wesley <wess.c@proton.me>
2026-06-22 10:40:15 -0300

    Load sidebar polish styles

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import "./styles.css";
 import "./stepAlignment.css";
 import "./compactLayout.css";
 import "./lessonReadability.css";
+import "./sidebarPolish.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
