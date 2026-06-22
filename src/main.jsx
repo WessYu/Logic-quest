ca13096debc52e1abe1877d75edc0de0a24f082f
Wesley <wess.c@proton.me>
2026-06-22 09:41:39 -0300

    Render PWA install prompt

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import GitHubPresence from "./GitHubPresence";
 import AccountManager from "./AccountManager";
+import PwaInstallPrompt from "./PwaInstallPrompt";
 import "./styles.css";
 import "./stepAlignment.css";
 import "./compactLayout.css";
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
     <GitHubPresence />
     <AccountManager />
+    <PwaInstallPrompt />
   </React.StrictMode>,
 );
 
